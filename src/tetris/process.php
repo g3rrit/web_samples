5904841e57846b94886df083829620bc35290469
pearisgreen <gerrit.proessl@gmail.com>
2019-02-08 22:34:29 +0100

    update

--- src/tetris/process.php
+++ src/tetris/process.php
@@ -1,48 +1,52 @@
 <?php
-   	
-   $myFile = "/home/gproessl/public_html/data.json";
-   $arr_data = array(); // create empty array
 
-  try
-  {
-	   //Get form data
-	   $formdata = array(
-	      'name'=> $_POST['name'],
-	      'score'=> $_POST['score'],
-	      'lvl'=>$_POST['lvl'],
-              'pw'=>$_POST['pw']
-	   );
+  $myFile = "/home/gproessl/public_html/data.json";
+  $arr_data = array(); // create empty array
 
-	   //Get data from existing json file
-	   $jsondata = file_get_contents($myFile);
+  function comparator($obja, $objb) {
+    return intval($obja['score']) <= intval($objb['score']);
+  }
 
-	   // converts json data into array
-	   $arr_data = json_decode($jsondata, true);
+  try {
+   //Get form data
+   $formdata = array(
+      'name'=> $_POST['name'],
+      'score'=> $_POST['score'],
+      'lvl'=>$_POST['lvl'],
+      'pw'=>$_POST['pw']
+   );
 
-           $new_array = array();
+   //Get data from existing json file
+   $jsondata = file_get_contents($myFile);
 
-           
-           foreach($arr_data as $user) {
-             if($user['pw'] == $formdata['pw']) {
-                            echo 'Success';
-               array_push($new_array, (object) array('name' => $formdata['name'], 'pw' => $formdata['pw'], 'score' => $formdata['score'], 'lvl' => $formdata['lvl']));
-             } else {
-               array_push($new_array, $user);
-             }
-           }
+   // converts json data into array
+   $arr_data = json_decode($jsondata, true);
 
-           //Convert updated array to JSON
-	   $jsondata = json_encode($new_array);
-	   
-	   //write json data into data.json file
-	   if(file_put_contents($myFile, $jsondata)) {
-	    }
-	   else 
-	        echo "error";
+   $new_array = array();
 
+   foreach($arr_data as $user) {
+     if($user['pw'] == strtoupper(hash('sha256', $formdata['pw']))) {
+       echo 'Success';
+       array_push($new_array, array('name' => $formdata['name'], 'pw' => strtoupper(hash('sha256', $formdata['pw'])), 'score' => $formdata['score'], 'lvl' => $formdata['lvl']));
+     } else {
+       array_push($new_array, $user);
+     }
    }
-   catch (Exception $e) {
-            echo 'Caught exception: ',  $e->getMessage(), "\n";
+
+   //sort array
+   usort($new_array, 'comparator');
+
+   //Convert updated array to JSON
+   $jsondata = json_encode($new_array);
+
+   //write json data into data.json file
+   if(file_put_contents($myFile, $jsondata)) {
+   } else {
+     echo "error";
+   }
+
+   } catch (Exception $e) {
+     echo 'Caught exception: ',  $e->getMessage(), "\n";
    }
 
 ?>
--- src/tetris/update.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php
-
-
-echo "Hi Mark";
-?>
